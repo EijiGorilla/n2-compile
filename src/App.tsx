fad60df94631b63220a4d82e689f966a339bb3c3
EijiGorilla <emasuz@hotmail.com>
2025-06-25 13:22:07 +0800

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useState } from "react";
+import "./App.css";
+import "./index.css";
+import "@arcgis/map-components/dist/components/arcgis-map";
+import "@arcgis/map-components/components/arcgis-map";
+import "@arcgis/map-components/components/arcgis-zoom";
+import "@arcgis/map-components/components/arcgis-legend";
+import "@esri/calcite-components/dist/components/calcite-shell";
+import "@esri/calcite-components/dist/calcite/calcite.css";
+import { CalciteShell } from "@esri/calcite-components-react";
+import MapDisplay from "./components/MapDisplay";
+import ActionPanel from "./components/ActionPanel";
+import Header from "./components/Header";
+import UndergroundSwitch from "./components/UndergroundSwitch";
+import { contractPackage } from "./Query";
+import { superurgent_items } from "./StatusUniqueValues";
+import MainChart from "./components/MainChart";
+
+type MyDropdownContextType = {
+  contractpackages: any;
+  updateContractPackage: any;
+  superurgentswitch: any;
+  updateSuperurgentSwitch: any;
+};
+
+const initialState = {
+  contractpackages: undefined,
+  updateContractPackage: undefined,
+  superurgentswitch: undefined,
+  updateSuperurgentSwitch: undefined,
+};
+
+export const MyContext = createContext<MyDropdownContextType>({
+  ...initialState,
+});
 
 function App() {
+  const [contractpackages, setContractpackages] = useState<any>(
+    contractPackage[0]
+  );
+
+  const updateContractPackage = (newContractpackage: any) => {
+    setContractpackages(newContractpackage);
+  };
+
+  const [superurgentswitch, setSuperurgentSwitch] = useState<any>(
+    superurgent_items[0]
+  );
+  const updateSuperurgentSwitch = (newSuperurgentButton: any) => {
+    setSuperurgentSwitch(newSuperurgentButton);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <CalciteShell>
+        <MyContext
+          value={{
+            contractpackages,
+            updateContractPackage,
+            superurgentswitch,
+            updateSuperurgentSwitch,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <ActionPanel />
+          <UndergroundSwitch />
+          <MapDisplay />
+          <MainChart />
+          <Header />
+        </MyContext>
+      </CalciteShell>
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,47 @@
+import ContractPackageDisplay from "./ContractPackageContext";
+
+function Header() {
+  return (
+    <>
+      <header
+        slot="header"
+        id="headerDiv"
+        style={{
+          display: "flex",
+          width: "100%",
+          height: "55px",
+          padding: "0.4rem",
+        }}
+      >
+        <img
+          src="https://EijiGorilla.github.io/Symbols/Projec_Logo/DOTr_Logo_v2.png"
+          alt="DOTr Logo"
+          height={"55px"}
+          width={"55px"}
+          style={{ marginBottom: "auto", marginTop: "auto" }}
+        />
+        <b className="headerTitle" style={{ width: "30%" }}>
+          N2 Overall Progress
+        </b>
+
+        {/* Contract Package Segmented List */}
+        <ContractPackageDisplay />
+
+        <img
+          src="https://EijiGorilla.github.io/Symbols/Projec_Logo/GCR LOGO.png"
+          alt="GCR Logo"
+          height={"50px"}
+          width={"75px"}
+          style={{
+            marginBottom: "auto",
+            marginTop: "auto",
+            marginLeft: "0.5rem",
+            marginRight: "2rem",
+          }}
+        />
+      </header>
+    </>
+  );
+}
+
+export default Header;
